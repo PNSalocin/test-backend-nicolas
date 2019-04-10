33e85e8dc4a8f190c84ce3765e1db2f3a7ad09a7
PNSalocin <nicolas.merelli@gmail.com>
2019-04-10 19:52:05 +0000

    doc(users): adding some user controller documentation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,9 @@ class UsersController < ApplicationController #:nodoc:
   has_scope :by_age_range, using: %i[minimum maximum], only: :index
 
   # GET /users
+  # GET /users?by_gender={gender}
+  # GET /users?by_age={age}
+  # GET /users?by_age_range[minimum]={minimum}&by_age_range[maximum]=maximum
   def index
     users = apply_scopes(User).all
     return head(:no_content) if users.blank?
